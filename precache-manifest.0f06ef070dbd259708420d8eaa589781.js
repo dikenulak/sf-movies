cc5607f7065e8182191a6146b46f7139b279580d
Diken Ulak <dikenulak@gmail.com>
2020-09-25 19:15:24 +0545

    Updates

---
+++ precache-manifest.0f06ef070dbd259708420d8eaa589781.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eebe6b49fb642dabfc5fd800cdd8c35",
+    "url": "/sf-movies/index.html"
+  },
+  {
+    "revision": "3c809bd66afc0dd567c6",
+    "url": "/sf-movies/static/css/main.10b30969.chunk.css"
+  },
+  {
+    "revision": "12f24dd154751366510e",
+    "url": "/sf-movies/static/js/2.1be25e78.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sf-movies/static/js/2.1be25e78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c809bd66afc0dd567c6",
+    "url": "/sf-movies/static/js/main.e6a55703.chunk.js"
+  },
+  {
+    "revision": "e4c7ea082ea7fdc2810c",
+    "url": "/sf-movies/static/js/runtime-main.82d44b1b.js"
+  },
+  {
+    "revision": "cefb903bedd34bae27aadba1f0eacc69",
+    "url": "/sf-movies/static/media/sanfrancisco.cefb903b.jpg"
+  }
+]);
